6bcc9e5498a7e3ec84df8bf3c02010f8953b7898
vinny@charmswraith <vincentdanso05@gmail.com>
2023-09-28 15:16:45 +0000

    bug fix

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -2,6 +2,10 @@
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+import Avatar1 from "../assets/Avatar1.svg";
+import Avatar2 from "../assets/Avatar2.svg";
+import Avatar3 from "../assets/Avatar3.svg";
+import Avatar4 from "../assets/quote.png";
 
 // Import Swiper styles
 import "swiper/css";
@@ -15,7 +19,7 @@ const reviews = [
     category: "Kindness",
     comment:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra.",
-    photo: "/src/assets/Avatar1.svg",
+    Avatar: Avatar1,
   },
   {
     id: 2,
@@ -24,7 +28,7 @@ const reviews = [
     category: "Humanity",
     comment:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra.",
-    photo: "/src/assets/Avatar2.svg",
+    Avatar: Avatar2,
   },
   {
     id: 3,
@@ -33,7 +37,7 @@ const reviews = [
     category: "Ethics & Morality",
     comment:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra.",
-    photo: "/src/assets/Avatar3.svg",
+    Avatar: Avatar3,
   },
 ];
 
@@ -65,13 +69,13 @@ const ReviewCard = () => {
             className="rounded border border-[#006B6A] p-8"
           >
             <div>
-              <img src="/src/assets/quote.png" alt="" className="h-4" />
+              <img src={Avatar4} alt="" className="h-4" />
               <h1 className="pt-4 font-['Sora'] text-[1.5rem]">
                 {review.category}
               </h1>
               <p className="my-5">{review.comment}</p>
               <div className="flex items-center gap-4">
-                <img src={review.photo} alt="" className="h-10" />
+                <img src={review.Avatar} alt="" className="h-10" />
                 <div>
                   <p>{review.name}</p>
                   <p className="text-sm">{review.occupation}</p>
